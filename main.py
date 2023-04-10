f244bc221d77c7028ed37d2e8a41bee3f97720af
Ian Sornson <iansornson@Ians-Laptop.local>
2023-04-09 20:30:06 -0500

    basic pygame stuff

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('first commit')
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((40, 40, 40))
+
+    pygame.display.flip()
+
+    clock.tick(60) 
+
+pygame.quit()
